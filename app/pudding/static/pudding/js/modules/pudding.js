eb8d4038e1bbf3ecfdd1dce058fc919b5ad6ddcb
Vitaliy Saveliev <hello@milkrage.ru>
2021-08-21 15:01:14 +0300

    add adaptive menu for tablet and mobile

--- app/pudding/static/pudding/js/modules/pudding.js
+++ app/pudding/static/pudding/js/modules/pudding.js
@@ -1,5 +1,10 @@
 class Pudding {
 
+	constructor() {
+		this.mobileMenu();
+		this.logout();
+	}
+
 	getMasterKey() {
 		let key = localStorage.getItem('key');
 
@@ -75,6 +80,50 @@ class Pudding {
 		}
 
 		return result;
+	}
+
+
+	logout() {
+		let logout = document.getElementById('logout');
+
+		if (logout !== null) {
+			logout.addEventListener('click', function(event) {
+				localStorage.removeItem('key');
+				logout.href = '/logout/';
+				logout.click();
+			});
+		}
+
+		this.mobileMenu;
+	}
+
+
+	mobileMenu() {
+		let show = document.getElementById('menu-show');
+		let close = document.getElementById('menu-close');
+
+		let main = document.getElementById('_main');
+		let header = document.getElementById('_header');
+
+		if (show !== null) {
+			show.addEventListener('click', function(event) {
+				header.style.display = 'flex';
+				document.querySelector('#_main .menu-btn').style.display = 'none';
+				document.body.style.width = '100%';
+				document.body.style.height = '100%';
+				document.body.style.overflow = 'hidden';
+			});
+		}
+
+		if (close !== null) {
+			close.addEventListener('click', function(event) {
+				header.style.display = 'none';
+				document.querySelector('#_main .menu-btn').style.display = 'block';
+				document.body.style.width = 'auto';
+				document.body.style.height = 'auto';
+				document.body.style.overflow = 'auto';
+			});
+		}
 	}
 
 }
